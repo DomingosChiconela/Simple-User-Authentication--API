0314e737d403d6bf92ae867a818529f6a7d47ae4
Domingos chiconela <azevedodomingos78@gmail.com>
2024-10-01 21:31:28 +0200

    feat:created the basic structure of the project and server.ts

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express,{ urlencoded }  from "express"
+import * as dotenv  from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+const app  =   express()
+
+const port =  process.env.PORT||3000
+
+app.use(cors())
+app.use(urlencoded({extended:true}))
+
+
+app.use( express.json())
+
+app.listen(port , ()=>{
+
+    console.log(` servidor rodando em http://localhost:${port}`)
+    
+    
+    })
+    
